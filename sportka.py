fabe67e52cf7359f02a6f1a263aeb446f7a70e53
Jakub Kolář <78088608+Jakewh@users.noreply.github.com>
2022-03-10 07:26:41 +0100

    windowed

--- sportka.py
+++ sportka.py
@@ -1,6 +1,10 @@
-import inquirer, random, re
+import tkinter as tk
+from tkinter import *
+import sys
+import random
 
-print(""" 
+
+""" 
                       _   _         
  ___ _ __   ___  _ __| |_| | ____ _ 
 / __| '_ \ / _ \| '__| __| |/ / _` |
@@ -8,33 +12,32 @@ print("""
 |___/ .__/ \___/|_|   \__|_|\_\__,_|
     |_|                             
                                                                
-""")
-opakovat = True
-while opakovat:
+"""
+okno = tk.Tk()
+okno.title("Generátor tiketů")
+
+def cely():
+    top = tk.Toplevel()
     cisla = list(range(1,50))
-    sloupec = 1
-    # začátek menu
-    otazka = [
-        inquirer.List("main_menu",
-        message="Co chceš losovat?",
-        choices=["Celý tiket", "Sloupec"],
-        ),
-    ]
-    odpoved = inquirer.prompt(otazka)
-    # konec menu
-    if odpoved == {'main_menu': 'Celý tiket'}:  # vyplnění celého tiketu
-        while sloupec <= 10:
-            print("Vylosovaná čísla jsou:")
-            print("Sloupec ", sloupec, random.sample(cisla, 6),"\n")
-            sloupec += 1
-            continue    
-    elif odpoved == {'main_menu': 'Sloupec'}:   # vyplnění jednoho sloupce
-        print("Vylosovaná čísla jsou:")
-        print(random.sample(cisla, 6))
-    znovu = input("Losovat znovu? ano/ne: ")
-    if znovu == "ano" or znovu == "":
-        continue
-    else:
-        break
-print("\n***Hacker Ninjas North 2022***")
-input()
+    Label(top, text=("Sloupec 1", random.sample(cisla, 6))).grid(row=0, column=0, sticky="w")
+    Label(top, text=("Sloupec 2", random.sample(cisla, 6))).grid(row=1, column=0, sticky="w")
+    Label(top, text=("Sloupec 3", random.sample(cisla, 6))).grid(row=2, column=0, sticky="w")
+    Label(top, text=("Sloupec 4", random.sample(cisla, 6))).grid(row=3, column=0, sticky="w")
+    Label(top, text=("Sloupec 5", random.sample(cisla, 6))).grid(row=4, column=0, sticky="w")
+    Label(top, text=("Sloupec 6", random.sample(cisla, 6))).grid(row=5, column=0, sticky="w")
+    Label(top, text=("Sloupec 7", random.sample(cisla, 6))).grid(row=6, column=0, sticky="w")
+    Label(top, text=("Sloupec 8", random.sample(cisla, 6))).grid(row=7, column=0, sticky="w")
+    Label(top, text=("Sloupec 9", random.sample(cisla, 6))).grid(row=8, column=0, sticky="w")
+    Label(top, text=("Sloupec 10", random.sample(cisla, 6))).grid(row=9, column=0, sticky="w")
+
+def sloupec():
+    top = tk.Toplevel()
+    cisla = list(range(1,50))
+    Label(top, text=random.sample(cisla, 6)).grid(row=0, column=0)
+
+Label(okno, text="Co chceš losovat?\n").grid(row=0, column=0, columnspan=2, sticky="we")
+cely = tk.Button(okno, text="Celý tiket", command=cely).grid(row=1, column=0)
+sloupec = tk.Button(okno, text="Pouze sloupec", command=sloupec).grid(row=1, column=1)
+
+okno.mainloop()
+
